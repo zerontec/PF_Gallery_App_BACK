b91e4cdd37e6a740eac41b5ef825c3964fd9c6b0
zerontec <zerontecnologia1@gmail.com>
2022-01-18 07:16:05 -0430

    agregue rutas sigup signin y rutas para usuarios segun roles

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,41 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const { verifySignUp, authJwt } = require("../middleware");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use('/products_apiDb', dogsRoutes);
 
+const { signup, signin } = require("../controllers/auth.controller");
 
+router.post(
+  "/auth/signup",
+  [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
+  signup
+);
+
+router.post("/auth/signin", signin);
+
+//para pruebas
+const {
+  allAccess,
+  userBoard,
+  vendedorBoard,
+  adminBoard,
+} = require("../controllers/user.controller");
+
+router.get("/test/all", allAccess);
+
+router.get("/test/user", [authJwt.verifyToken], userBoard);
+
+router.get(
+  "/test/vend",
+  [authJwt.verifyToken, authJwt.isVendedor],
+  vendedorBoard
+);
+
+router.get("/test/admin", [authJwt.verifyToken, authJwt.isAdmin], adminBoard);
 
 module.exports = router;
