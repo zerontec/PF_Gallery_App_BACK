f2df55d3f5a6aff37b4e4527b3843edf47d91a79
zerontec <zerontecnologia1@gmail.com>
2022-01-18 06:45:03 -0430

    agrege funcion initial para agregar roles a db

--- index.js
+++ index.js
@@ -19,12 +19,32 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {Role }= require ('./src/db')
+const ROLE=["user", "vendedor", "admin", ];
 
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "vendedor"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(5040, () => {
-    console.log('o|O_O|o robot Σωκράτης listening at 5040'); // eslint-disable-line no-console
+    console.log('o|O_O|o robot Σωκράτης listening at 5040');
+    initial();
+  // eslint-disable-line no-console
   });
   // getData(); <<<--- para iniciar la base de datos
 });
