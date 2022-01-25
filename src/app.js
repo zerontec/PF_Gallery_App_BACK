9c2360b6675d09a59e09bbf29c3b3d91ae8a1446
Mauricio776 <mauuuricio@hotmail.com>
2022-01-24 23:13:02 -0300

    start change5

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
+  
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Origin', '*'); // habilita a cualquir dire front
   res.header('Access-Control-Allow-Credentials', 'true');
